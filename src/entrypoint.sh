dac326c60a4cf710aa8bff52745bb023103da7cf
Steve Szabo <steve.g.szabo@gmail.com>
2025-02-02 07:36:23 -0500

    updates

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-#set -o errexit
-#set -o pipefail
-
-VAULT_ENVIRONMENT=${VAULT_ENVIRONMENT-eks-engineering-01}
-VAULT_SECRET_MANAGER=$VAULT_ENVIRONMENT-vault
-
-VAULT_INIT_FILE=$HOME/$VAULT_ENVIRONMENT
-VAULT_VIRTUAL=$HOME/environment/bin/activate
-
-VAULT_FILTER=$(printf '.SecretList[]|select(.Name=="%s")|.ARN' $VAULT_SECRET_MANAGER)
-
-VAULT_RECOVER_INDEX=1
+export VAULT_ADDR=https://127.0.0.1:8200
 VAULT_SHUTDOWN=0
-
-[ -f $VAULT_VIRTUAL ] && source $VAULT_VIRTUAL
-
-export VAULT_ADDR=http://localhost:8200/
 
 trap "VAULT_SHUTDOWN=1" SIGINT SIGTERM
 
@@ -35,27 +20,6 @@ do
     printf "Vault currently initialized: [%s]\n" "$VAULT_STATUS_INIT"
     sleep 5
     continue
-  fi
-
-  vault operator init > $VAULT_INIT_FILE
-
-  VAULT_TOKEN="$(grep 'Initial Root Token' $VAULT_INIT_FILE | awk -F: '{print $NF}' | tr -d ' ')"
-  VAULT_SECRET_VALUE=$(printf '"root": "%s"' "$VAULT_TOKEN")
-
-  for VAULT_RECOVER_KEY in $(grep 'Recovery Key' $VAULT_INIT_FILE | awk '{print $NF}' | tr -d ' ')
-  do
-    VAULT_SECRET_VALUE=$(printf '%s, "recovery-%d": "%s"' "$VAULT_SECRET_VALUE" "$VAULT_RECOVER_INDEX" "$VAULT_RECOVER_KEY")
-    VAULT_RECOVER_INDEX=$((VAULT_RECOVER_INDEX+1))
-  done
-
-  VAULT_SECRET_ARN=$(aws secretsmanager list-secrets | jq -r "$VAULT_FILTER")
-
-  if [ -z "$VAULT_SECRET_ARN" ]; then
-    printf "Create secret: [%s]\n" "$VAULT_SECRET_MANAGER"
-    aws secretsmanager create-secret --name "$VAULT_SECRET_MANAGER" --secret-string "{$VAULT_SECRET_VALUE}"
-  else
-    printf "Update secret: [%s]\n" "$VAULT_SECRET_MANAGER"
-    aws secretsmanager update-secret --secret-id $VAULT_SECRET_ARN --secret-string "{$VAULT_SECRET_VALUE}"
   fi
 
 done

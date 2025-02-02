092734a745b8a0504b91bc6f8ec0e4d8a6a5d929
Steve Szabo <steve.g.szabo@gmail.com>
2025-02-02 07:53:53 -0500

    updates

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export VAULT_ADDR=https://127.0.0.1:8200
 VAULT_SHUTDOWN=0
 
 trap "VAULT_SHUTDOWN=1" SIGINT SIGTERM
@@ -8,19 +7,22 @@ trap "VAULT_SHUTDOWN=1" SIGINT SIGTERM
 while [ $VAULT_SHUTDOWN -eq 0 ];
 do
 
-  VAULT_STATUS_INIT=$(vault status -format=json | jq -r .initialized)
+    VAULT_STATUS_INIT=$(vault status -format=json | jq -r .initialized)
 
-  if [ -z "$VAULT_STATUS_INIT" ]; then
-    printf "Unable to determine vault init state: vault service running?\n"
-    sleep 5
-    continue
-  fi
+    if [ -z "$VAULT_STATUS_INIT" ]; then
+        printf "Unable to determine vault init state: vault service running?\n"
+        sleep 5
+        continue
+    fi
 
-  if [ "$VAULT_STATUS_INIT" = "true" ]; then
-    printf "Vault currently initialized: [%s]\n" "$VAULT_STATUS_INIT"
+    if [ "$VAULT_STATUS_INIT" = "true" ]; then
+        printf "Vault currently initialized: [%s]\n" "$VAULT_STATUS_INIT"
+        sleep 5
+        continue
+    fi
+
+    printf "Initializing Vault\n"
     sleep 5
-    continue
-  fi
 
 done
 
